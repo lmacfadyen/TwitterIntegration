62db5ec1550bce7da215e8211096d8fd6b228f41
Lawrence F MacFadyen <larry.macfadyen@gmail.com>
2015-07-24 07:54:55 -0400

    Initial Commit

---
+++ TwitterIntegration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterIntegration
+//
+//  Created by Lawrence F MacFadyen on 2015-07-24.
+//  Copyright (c) 2015 LawrenceM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
